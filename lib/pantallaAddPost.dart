89e46e4112e81e2129f48c50d94c35ddd521aab6
AdrianGavilanGomez <adriangavilangomez@gmail.com>
2023-05-19 19:13:31 +0200

    ola

--- lib/pantallaAddPost.dart
+++ lib/pantallaAddPost.dart
@@ -17,55 +17,100 @@ class _PantallaAddPostState extends State<PantallaAddPost> {
     return Scaffold(
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(vertical: 40),
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/letras_yourss.png',
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'SUBIR PUBLICACIÓN',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // Centra verticalmente
+            crossAxisAlignment: CrossAxisAlignment.center, // Centra horizontalmente
+            children: [
+              Image.asset(
+                'assets/images/letras_yourss.png',
               ),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              '¡QUE TODO EL MUNDO SE ENTERE!',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const Text(
-              'Ivan Gallego',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar el buton
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PantallaLogin()),
-                );
-              },
-              child: const Text('CERRAR SESIÓN'),
-              style: ElevatedButton.styleFrom(
-                textStyle: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 15,
+              const SizedBox(height: 10),
+              const Text(
+                'SUBIR PUBLICACIÓN',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            const SizedBox(height: 25),
-            const Divider(),
-          ],
+              Container(
+                padding: const EdgeInsets.all(30),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '¡QUE TODO EL MUNDO SE ENTERE!',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          TextField(
+                            maxLines: 4,
+                            textAlign: TextAlign.left,
+                            textAlignVertical: TextAlignVertical.top,
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+                              hintText: ' Cuenta a otros viajeros tu experiencia',
+                              border: OutlineInputBorder(),
+                              alignLabelWithHint: true,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.camera_alt),
+                              onPressed: () {
+                                // Lógica para abrir la cámara aquí
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Spacer(),
+                      IconButton(
+                        icon: Icon(Icons.image_search),
+                        onPressed: () {
+                          // Lógica para abrir la galeria aquí
+                        },
+                      ),
+                      const Spacer(),
+                      Image.asset(
+                        'assets/images/sagrada_familiaa.png',
+                        width: 10,
+                        height: 10,
+                      ),
+                      const Spacer(),
+                      Image.asset(
+                        'assets/images/maremagnum.png',
+                        width: 10,
+                        height: 10,
+                      ),
+                    ],
+                  )
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
